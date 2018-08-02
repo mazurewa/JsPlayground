83926cef7e51edc00dfe95a15284b526f96f7853
mazurewa <mazurewa@users.noreply.github.com>
2018-08-02 14:46:37 +0200

    Practicing string operations

--- Data types/Strings/functions.js
+++ Data types/Strings/functions.js
@@ -3,3 +3,18 @@ function ucFirst(text) {
 	return newText;
 }
 
+function checkSpam(text) {
+	return text.toLowerCase().includes("viagra") || text.toLowerCase().includes("XXX".toLowerCase());
+}
+
+function truncate(str, maxlength) {
+	if (str.length <= maxlength) {
+		return str;
+	} else {
+		return str.substr(0, maxlength-1) + "…";
+	}
+}
+
+function extractCurrencyValue(str) {
+	return parseInt(str.slice(1));
+}
--- Data types/Strings/ucFirstTests.js
+++ Data types/Strings/ucFirstTests.js
@@ -8,3 +8,28 @@ describe("ucFirst", function() {
 	});
 });	
 
+describe("checkSpam", function() {
+	it('finds spam in "free xxxxx"', function() {
+		assert.isTrue(checkSpam('free xxxxx'))
+	});
+	
+	it('no spam in "innocent rabbit"', function() {
+		assert.isFalse(checkSpam("innocent rabbit"))
+	});
+});
+
+describe("truncate", function() {
+	it('truncate the long string to the given length (including the ellipsis)', function() {
+	assert.equal(truncate('Everything is everything, after winter must come spring', 30), 'Everything is everything, aft…')});
+	
+	it('doesn\'t change short strings', function() {
+	assert.equal(truncate('Lauryn Hill', 15), 'Lauryn Hill')});
+});
+	
+describe('extractCurrencyValue', function() {
+	it("for the string $120 returns the number 120", function() {
+    assert.strictEqual(extractCurrencyValue('$120'), 120);
+  });
+});
+
+	
