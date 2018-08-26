0030df2ed33c954f1ae7f631ff8fbb3d733bc209
Ewa Mazur <emazuroo@gmail.com>
2018-08-26 21:57:52 +0200

    Prototypes

---
+++ Objects/Prototypes/functions.js
@@ -0,0 +1,11 @@
+Function.prototype.defer = function(ms) {
+				setTimeout(this, ms);
+			}
+			
+Function.prototype.deferSum = function(ms) {
+	let outerThis = this;
+	return function(...args) {
+		setTimeout(outerThis.apply(null, args), ms);
+	}
+}
+			
